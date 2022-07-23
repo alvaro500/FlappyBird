a63bc26731e5cffaa6894fd45069386e43824923
unknown <alvaro500@outlook.com>
2022-07-23 14:22:26 -0500

    Instanciación separada por tiempo

--- Assets/Scripts/ObstaclePool.cs
+++ Assets/Scripts/ObstaclePool.cs
@@ -6,7 +6,12 @@ public class ObstaclePool : MonoBehaviour
 {
     [SerializeField] private GameObject obstaclePrefab;
     [SerializeField] private int poolSize = 5;
+    [SerializeField] private float spawnTime = 2.5f;
+
+    private float timeElapsed; //Guardar el tiempo que pasó desde la última aparición
+    private int obstacleCount;
     private GameObject[] obstacles;
+    
 
     // Start is called before the first frame update
     void Start()
@@ -17,14 +22,28 @@ public class ObstaclePool : MonoBehaviour
         {
             obstacles[i] = Instantiate(obstaclePrefab);
             obstacles[i].SetActive(false);
-
-
         }
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        timeElapsed += Time.deltaTime;
+        if (timeElapsed > spawnTime)
+        {
+            SpawnObstacle();
+        }        
+    }
+
+    private void SpawnObstacle()
+    {
+        timeElapsed = 0f;
+        obstacles[obstacleCount].SetActive(true);
+        obstacleCount++;
+
+        if (obstacleCount == poolSize)
+        {
+            obstacleCount = 0;
+        }
     }
 }
